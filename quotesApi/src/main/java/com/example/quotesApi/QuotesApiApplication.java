7e9a619ca3b038fc3e59885ead46a014a2542d70
dissemblable <julesvallet23@gmail.com>
2025-02-13 10:08:13 +0100

    commit d'initialisation des deux projets

---
+++ quotesApi/src/main/java/com/example/quotesApi/QuotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.quotesApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesApiApplication.class, args);
+	}
+
+}
